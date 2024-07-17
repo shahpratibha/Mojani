0514222a342f22b34c3c7546c1cf9b2bb0d3c41e
shahpratibha <pratibhashah1995@gmail.com>
2024-07-17 12:51:20 +0530

    add

--- db.php
+++ db.php
@@ -1,9 +1,24 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $host = "157.173.222.9";
 $port = "5432";
 $dbname = "Mojani";
 $user = "postgres";
 $password = "Geo992101";
+
+
+// $host = "rr.c01x1jtcm1ms.ap-south-1.rds.amazonaws.com";
+// $port = "5432";
+// $dbname = "Mojani";
+// $user = "postgres";
+// $password = "Pmc992101";
+
+
 
 
 try {
--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header('Location: Login.php');
+header('Location: login.php');
 exit;
 ?>
