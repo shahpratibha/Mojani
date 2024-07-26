f737d67753ead5175c422eaa46a9176ceef1dcb1
shahpratibha <pratibhashah1995@gmail.com>
2024-07-26 14:37:33 +0530

    add time according to local time

--- Adminregister_process.php
+++ Adminregister_process.php
@@ -1,4 +1,6 @@
 <?php
+
+date_default_timezone_set('UTC');
 include('db.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -8,7 +10,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $occupation = $_POST["occupation"];
     $contact_no = $_POST["contact_no"];
 
-    $stmt = $pdo->prepare("INSERT INTO admin_users (username, email, password, occupation, contact_no) VALUES (?, ?, ?, ?, ?)");
+    // $stmt = $pdo->prepare("INSERT INTO admin_users (username, email, password, occupation, contact_no) VALUES (?, ?, ?, ?, ?)");
+
+    $stmt = $pdo->prepare("INSERT INTO admin_users (username, email, password, occupation, contact_no, user_entry_timestamp) 
+    VALUES (?, ?, ?, ?, ?, NOW() AT TIME ZONE 'Asia/Kolkata')");
     $stmt->execute([$username, $email, $password, $occupation, $contact_no]);
     // Redirect to form.php after successful registration
     header("Location: admin.php");
--- register_process.php
+++ register_process.php
@@ -1,4 +1,6 @@
 <?php
+
+date_default_timezone_set('UTC');
 include('db.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -8,8 +10,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $occupation = $_POST["occupation"];
     $contact_no = $_POST["contact_no"];
 
-    $stmt = $pdo->prepare("INSERT INTO users (username, email, password, occupation, contact_no) VALUES (?, ?, ?, ?, ?)");
+    // Prepare the SQL statement with a placeholder for the current timestamp
+    $stmt = $pdo->prepare("
+        INSERT INTO users (username, email, password, occupation, contact_no, user_entry_timestamp) 
+        VALUES (?, ?, ?, ?, ?, NOW() AT TIME ZONE 'Asia/Kolkata')
+    ");
     $stmt->execute([$username, $email, $password, $occupation, $contact_no]);
+    
     // Redirect to form.php after successful registration
     header("Location: login.php");
     exit();
